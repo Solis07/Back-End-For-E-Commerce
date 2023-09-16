c6ca44fb720d74447754230709b6bce63156cc6b
L.J Solis <l.jsolis52@yahoo.com>
2023-09-16 09:29:07 -0500

    Finished the GET method for the find all and find one tag for the tag routes.js.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,15 +6,57 @@ const { Tag, Product, ProductTag } = require("../../models");
 router.get("/", (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  
+  Tag.findAll({
+    attributes: ["id", "tag_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+        through: ProductTag,
+        as: "Products"
+      }
+    ]
+  })
+    .then((dbTagData) => res.json(dbTagData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  
+  Tag.findOne({
+    where: { id: req.params.id },
+    attributes: ["id", "tag_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+        through: ProductTag,
+        as: "Products"
+      }
+    ]
+  })
+    .then((dbTagData) => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "Could not find tag with this id" });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post("/", (req, res) => {
   // create a new tag
+  
   Tag.create({
     tag_name: req.body.tag_name,
   })
@@ -51,6 +93,7 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  
   Tag.destroy({
     where: { id: req.params.id },
   })
